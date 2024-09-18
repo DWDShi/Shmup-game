4eaa058d4ce5ff7e420d5cc0c1a2475ce205b7ea
Chimali <64831405+ChimalXG@users.noreply.github.com>
2024-09-18 17:15:32 +0100

    Simple player movement
    Made a simple, basic controller for the player which moves the player ship with no regards for acceleration etc, can be changed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,18 +9,44 @@ namespace Shmup
     {
         [SerializeField]
         private float moveSpeed = 3.0f;
+        [SerializeField]
+        private float rateOfFire = 2.0f;
 
+        [SerializeField]
+        private InputReader input;
+
+        private Vector2 moveDirection;
+        private bool isShooting;
 
         // Start is called before the first frame update
         void Start()
         {
+            input.MoveEvent += MovePlayer;
+            input.ShootEvent += firePlayerWeapon;
+        }
 
+        private void firePlayerWeapon()
+        {
+            isShooting = true;
+        }
+
+        private void MovePlayer(Vector2 newDirection)
+        {
+            moveDirection = newDirection;
+        }
+
+        private void ExecuteMovement() 
+        {
+            if (moveDirection != Vector2.zero) 
+            {
+                transform.position += (new Vector3(moveDirection.x, moveDirection.y, 0) * (moveSpeed * Time.deltaTime)) ; 
+            }
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            ExecuteMovement();
         }
     }
 }
